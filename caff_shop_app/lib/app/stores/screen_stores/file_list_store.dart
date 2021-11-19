65e17bc26ecaf85387d322391782be69c846f901
Csongi <szabocsongi576@gmail.com>
2021-11-19 19:45:44 +0100

    App - Layout fixes, caff browser impl

---
+++ caff_shop_app/lib/app/stores/screen_stores/file_list_store.dart
@@ -0,0 +1,39 @@
+import 'package:caff_shop_app/app/stores/widget_stores/loading_store.dart';
+import 'package:mobx/mobx.dart';
+
+part 'file_list_store.g.dart';
+
+class FileListStore = _FileListStore with _$FileListStore;
+
+abstract class _FileListStore with Store {
+  final LoadingStore loadingStore = LoadingStore();
+
+  // store variables:-----------------------------------------------------------
+  @observable
+  ObservableList<String> caffSrcList = ObservableList.of([]);
+
+  // actions:-------------------------------------------------------------------
+  @action
+  Future<void> getCaffFiles({
+    required void Function(String) onError,
+  }) async {
+    loadingStore.loading = true;
+
+    await Future.delayed(Duration(milliseconds: 500));
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+    caffSrcList.add('https://via.placeholder.com/150');
+
+    loadingStore.loading = false;
+  }
+
+  // general methods:-----------------------------------------------------------
+}
