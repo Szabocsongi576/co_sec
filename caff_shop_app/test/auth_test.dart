046df6a38dd6747f071f3fd9f23272c6ef534369
franciskav <franciska.vigh@gmail.com>
2021-12-05 20:28:49 +0100

    fix tests

--- caff_shop_app/test/auth_test.dart
+++ caff_shop_app/test/auth_test.dart
@@ -1,23 +1,26 @@
+import 'package:caff_shop_app/app/stores/screen_stores/login_store.dart';
 import 'package:caff_shop_app/app/stores/screen_stores/register_store.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  RegisterStore registerStore = RegisterStore();
+  LoginStore loginStore = LoginStore();
+
+  const userNameRight = 'Teszt Felhasználó';
+  const userNameWrong = '.-!£';
+  const emailRight = 'test@mail.hu';
+  const emailWrong1 = 'asd';
+  const emailWrong2 = 'a@b@c.d';
+  const passwordRight = 'Password12.';
+  const password = 'pass';
+  const passwordConfirmation = 'word';
+  const empty = '';
+
+  const error_field_is_empty = 'error.field_is_empty';
+  const error_invalid_content = 'error.invalid_content';
+  const error_passwords_different = 'error.passwords_different';
+
   group('Test register store', () {
-    RegisterStore registerStore = RegisterStore();
-
-    const userNameRight = 'Teszt Felhasználó';
-    const userNameWrong = '.-!£';
-    const emailRight = 'test@mail.hu';
-    const emailWrong1 = 'asd';
-    const emailWrong2 = 'a@b@c.d';
-    const passwordRight = 'Password12.';
-    const password = 'pass';
-    const passwordConfirmation = 'word';
-    const empty = '';
-
-    const error_field_is_empty = 'error.field_is_empty';
-    const error_invalid_content = 'error.invalid_content';
-    const error_passwords_different = 'error.passwords_different';
 
     test('Validation right input', () {
       registerStore.username = userNameRight;
@@ -77,6 +80,25 @@ void main() {
       expect(registerStore.emailError, null);
       expect(registerStore.passwordError, null);
       expect(registerStore.passwordConfirmationError, error_passwords_different);
+    });
+  });
+
+  group('Test login store', () {
+
+    test('Validation right input', () {
+      loginStore.username = userNameRight;
+      loginStore.password = passwordRight;
+      expect(loginStore.validate(), true);
+      expect(loginStore.usernameError, null);
+      expect(loginStore.passwordError, null);
+    });
+
+    test('Validation empty inputs', () {
+      loginStore.username = empty;
+      loginStore.password = empty;
+      expect(loginStore.validate(), false);
+      expect(loginStore.usernameError, error_field_is_empty);
+      expect(loginStore.passwordError, error_field_is_empty);
     });
   });
 }
