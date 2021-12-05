48f507cff1742a55ef022502e5441cd9da82866e
Hadarics Szabolcs <hasza98@gmail.com>
2021-12-05 02:30:20 +0100

    Added Comment documentation to Test Files

--- backend/src/test/java/com/cosec/backend/controllers/CaffControllerTest.java
+++ backend/src/test/java/com/cosec/backend/controllers/CaffControllerTest.java
@@ -68,6 +68,18 @@ class CaffControllerTest {
     @MockBean
     JwtUtils jwtUtils;
 
+    /**
+     * Description:
+     *      Checking if getting all the Caffs from the db returns correct response.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return the list of Caff's.
+     * Request:
+     *      GET(/caffs/unauth/all)
+     * Input:
+     *      -
+     * Expected output:
+     *      List of Caffs, which have been wrapped with CaffResponse.
+     */
     @Test
     void getAll_Success() throws Exception {
         Caff caffOne = new Caff("a","b",new Binary(new byte[0]));
@@ -80,6 +92,18 @@ class CaffControllerTest {
                 .andExpect(content().string("[{\"id\":\"1\",\"userId\":\"a\",\"name\":\"b\",\"imageUrl\":\"/caffs/unauth/image/1\"},{\"id\":\"2\",\"userId\":\"a\",\"name\":\"b\",\"imageUrl\":\"/caffs/unauth/image/2\"}]"));
     }
 
+    /**
+     * Description:
+     *      Checking if getting from empty db returns the correct error.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return the list of Caff's. Which is 0.
+     * Request:
+     *      GET(/caffs/unauth/all)
+     * Input:
+     *      -
+     * Expected output:
+     *      Bad request, with message: Caff database is empty!
+     */
     @Test
     void getAll_DB_IsEmpty() throws Exception {
         when(caffRepository.findAll()).thenReturn(List.of());
@@ -88,6 +112,19 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"message\":\"Error: Caff database is empty!\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if getting a Caff by its ID returns the Caff.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return true for checking existence of the Caff.
+     *      Upon searching for the caff, it should return the Caff as an optional value.
+     * Request:
+     *      GET(/caffs/unauth/1)
+     * Input:
+     *      Path Parameter ID (1)
+     * Expected output:
+     *      Caff object wrapped in CaffResponse class.
+     */
     @Test
     void getCaffById_Success() throws Exception {
         Caff caffOne = new Caff("a","b",new Binary(new byte[0]));
@@ -99,6 +136,18 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"id\":\"1\",\"userId\":\"a\",\"name\":\"b\",\"imageUrl\":\"/caffs/unauth/image/1\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if getting a Caff by its ID, when Caff with specified ID doesnt exist.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return false for finding the caff.
+     * Request:
+     *      GET(/caffs/unauth/1)
+     * Input:
+     *      Path Parameter ID (1)
+     * Expected output:
+     *      Bad request, with message: Caff does not exist!
+     */
     @Test
     void getCaffById_CaffDoesntExist() throws Exception {
         Caff caffOne = new Caff("a","b",new Binary(new byte[0]));
@@ -109,6 +158,19 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"message\":\"Error: Caff does not exist!\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if downloading a Caff by its ID, returns the file.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return true for finding the caff.
+     *      Upon findById it should return the Caff wrapped as Optional.
+     * Request:
+     *      GET(/caffs/unauth/1/download)
+     * Input:
+     *      Path Parameter ID (1)
+     * Expected output:
+     *      The File returned in the response as a Multipart File (Signaled in header).
+     */
     @Test
     void downloadCaffById_Success() throws Exception {
         Caff caffOne = new Caff("a","b",new Binary("ADAT".getBytes(StandardCharsets.UTF_8)));
@@ -122,6 +184,18 @@ class CaffControllerTest {
                 .andExpect(content().string("ADAT"));
     }
 
+    /**
+     * Description:
+     *      Checking if downloading a Caff by its ID, returns an error, when the Caff doesnt exist.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return false for finding the caff.
+     * Request:
+     *      GET(/caffs/unauth/1/download)
+     * Input:
+     *      Path Parameter ID (1)
+     * Expected output:
+     *      Bad request, with message: Caff does not exist!
+     */
     @Test
     void downloadCaffById_CaffDoesntExist() throws Exception {
         when(caffRepository.existsById("1")).thenReturn(false);
@@ -130,6 +204,19 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"message\":\"Error: Caff does not exist!\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if searching for a Caffs by its Name, returns the correct Caffs.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return true for finding any caffs.
+     *      Upon findByName it should return the List of Caffs.
+     * Request:
+     *      GET(/caffs/unauth/search)
+     * Input:
+     *      Request Parameter name (SomeName)
+     * Expected output:
+     *      A List of Caffs/Caff with the matching name wrapped in CaffResponse Objects.
+     */
     @Test
     void searchCaffByName_Success() throws Exception {
         Caff caffOne = new Caff("a","SomeName",new Binary(new byte[0]));
@@ -141,6 +228,18 @@ class CaffControllerTest {
                 .andExpect(content().string("[{\"id\":\"1\",\"userId\":\"a\",\"name\":\"SomeName\",\"imageUrl\":\"/caffs/unauth/image/1\"}]"));
     }
 
+    /**
+     * Description:
+     *      Checking if searching for a Caffs by its Name, returns the correct error if the name doesnt exist.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return false for finding any caffs.
+     * Request:
+     *      GET(/caffs/unauth/search)
+     * Input:
+     *      Request Parameter name (SomeName)
+     * Expected output:
+     *      Bad request, with message: Caff does not exist with this name!
+     */
     @Test
     void searchCaffByName_CaffnotFound() throws Exception {
         when(caffRepository.existsByName("SomeName")).thenReturn(false);
@@ -149,6 +248,19 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"message\":\"Error: Caff does not exist with this name!\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if getting all the Comments for a given Caff returns the Comments.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return true for finding the caff with the specified id.
+     *      CommentRepository is mocked, it should return the List of Comments for the given Caff.
+     * Request:
+     *      GET(/caffs/unauth/1/comments)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      List of Comments, that belong to the specified Caff. (CaffId = 1)
+     */
     @Test
     void getAllComment_Success() throws Exception {
         Comment comm1 = new Comment("1", "2", "1", "Comment");
@@ -160,6 +272,18 @@ class CaffControllerTest {
                 .andExpect(content().string("[{\"id\":\"1\",\"userId\":\"2\",\"caffId\":\"1\",\"text\":\"Comment\"},{\"id\":\"2\",\"userId\":\"3\",\"caffId\":\"1\",\"text\":\"Other Comment\"}]"));
     }
 
+    /**
+     * Description:
+     *      Checking if getting all the Comments for a given Caff returns error, if the Caff doesnt exist
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return false for finding the caff with the specified id.
+     * Request:
+     *      GET(/caffs/unauth/1/comments)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Bad request, with message: Caff does not exist !
+     */
     @Test
     void getAllComment_CaffnotFound() throws Exception {
         when(caffRepository.existsById("1")).thenReturn(false);
@@ -168,6 +292,19 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"message\":\"Error: Caff does not exist!\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if creating a comment for a Caff gets saved in the Database.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return true for finding the caff with the specified id.
+     *      User must be logged in.
+     * Request:
+     *      POST(/caffs/auth/comments)
+     * Input:
+     *      Request Body Comment (comm1)
+     * Expected output:
+     *      The created comment returned.
+     */
     @Test
     void createComment_Success() throws Exception {
         Comment comm1 = new Comment( "2", "1", "Comment");
@@ -182,6 +319,19 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"id\":null,\"userId\":\"2\",\"caffId\":\"1\",\"text\":\"Comment\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if creating a comment for a Caff returns the correct error if the Caff doesnt exist.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return false for finding the caff with the specified id.
+     *      User must be logged in.
+     * Request:
+     *      POST(/caffs/auth/comments)
+     * Input:
+     *      Request Body Comment (comm1)
+     * Expected output:
+     *      Bad request, with message: Caff does not exist !
+     */
     @Test
     void createComment_CaffnotFound() throws Exception {
         Comment comm1 = new Comment( "2", "1", "Comment");
@@ -196,6 +346,18 @@ class CaffControllerTest {
                 .andExpect(content().string("{\"message\":\"Error: Caff does not exist!\"}"));
     }
 
+    /**
+     * Description:
+     *      Checking if unauthorized user is unable to create a comment.
+     * Mock Restrictions:
+     *      -
+     * Request:
+     *      GET(/caffs/auth/comments)
+     * Input:
+     *      -
+     * Expected output:
+     *      Forbidden Response Value.
+     */
     @Test
     void createComment_Unauthorized() throws Exception {
         UserDetailsImpl user = new UserDetailsImpl(null, null, null, null, null);
@@ -204,6 +366,21 @@ class CaffControllerTest {
                 .andExpect(status().isForbidden());
     }
 
+    /**
+     * Description:
+     *      Checking if deleting a Caff has the correct effect on the Database, and returns the correct response.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return true for finding the caff with the specified id.
+     *      User must be logged in. User must have ADMIN role.
+     * Request:
+     *      DELETE(/caffs/admin/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Ok, with message: Caff deleted successfully!
+     *      CaffRepository.deleteById Called only once with argument "1"
+     *      CommentRepository.deleteAllByCaffId Called only once with argument "1"
+     */
     @Test
     void deleteCaffById_Success() throws Exception {
         UserDetailsImpl user = new UserDetailsImpl("1", "hasza98", "hasza98@gmail.com", "password", AuthorityUtils.createAuthorityList("ROLE_ADMIN"));
@@ -216,6 +393,19 @@ class CaffControllerTest {
         verify(commentRepository,times(1)).deleteAllByCaffId("1");
     }
 
+    /**
+     * Description:
+     *      Checking if deleting a Caff returns the correct error if the Caff doesnt exist.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return false for finding the caff with the specified id.
+     *      User must be logged in. User must have ADMIN role.
+     * Request:
+     *      DELETE(/caffs/admin/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Bad request, with message: Caff does not exist !
+     */
     @Test
     void deleteCaffById_CaffnotFound() throws Exception {
         UserDetailsImpl user = new UserDetailsImpl("1", "hasza98", "hasza98@gmail.com", "password", AuthorityUtils.createAuthorityList("ROLE_ADMIN"));
@@ -225,7 +415,18 @@ class CaffControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string("{\"message\":\"Error: Caff does not exist!\"}"));
     }
-
+    /**
+     * Description:
+     *      Checking if deleting a Caff returns the correct error if the User is not authorized.
+     * Mock Restrictions:
+     *      -
+     * Request:
+     *      DELETE(/caffs/admin/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Forbidden Response Value.
+     */
     @Test
     void deleteCaffById_Unauthorized() throws Exception {
         UserDetailsImpl user = new UserDetailsImpl("1", "hasza98", "hasza98@gmail.com", "password", AuthorityUtils.createAuthorityList("ROLE_USER"));
@@ -234,6 +435,20 @@ class CaffControllerTest {
                 .andExpect(status().isForbidden());
     }
 
+    /**
+     * Description:
+     *      Checking if deleting a Comment has the correct effect on the Database, and returns the correct response.
+     * Mock Restrictions:
+     *      CommentRepository is mocked, it should return true for finding the comment.
+     *      User must be logged in. User must have ADMIN role.
+     * Request:
+     *      DELETE(/caffs/admin/coments/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Ok, with message: Comment deleted successfully!
+     *      commentRepository.deleteById Called only once with argument "1"
+     */
     @Test
     void deleteCommentById_Success() throws Exception {
         UserDetailsImpl user = new UserDetailsImpl("1", "hasza98", "hasza98@gmail.com", "password", AuthorityUtils.createAuthorityList("ROLE_ADMIN"));
@@ -245,6 +460,20 @@ class CaffControllerTest {
         verify(commentRepository, times(1)).deleteById("1");
     }
 
+    /**
+     * Description:
+     *      Checking if deleting a Comment by Id returns the correct error, if the comment doesnt exist.
+     * Mock Restrictions:
+     *      CommentRepository is mocked, it should return false for finding the comment.
+     *      User must be logged in. User must have ADMIN role.
+     * Request:
+     *      DELETE(/caffs/admin/coments/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Bad request, with message: Comment does not exist!
+     *      commentRepository.deleteById never called with argument "1"
+     */
     @Test
     void deleteCommentById_CommentNotFound() throws Exception {
         UserDetailsImpl user = new UserDetailsImpl("1", "hasza98", "hasza98@gmail.com", "password", AuthorityUtils.createAuthorityList("ROLE_ADMIN"));
@@ -256,6 +485,18 @@ class CaffControllerTest {
         verify(commentRepository, never()).deleteById("1");
     }
 
+    /**
+     * Description:
+     *      Checking if deleting a Comment by Id returns the correct error, if the user is not authorized.
+     * Mock Restrictions:
+     *      -
+     * Request:
+     *      DELETE(/caffs/admin/coments/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Forbidden Response Value.
+     */
     @Test
     void deleteCommentById_Unauthorized() throws Exception {
         UserDetailsImpl user = new UserDetailsImpl("1", "hasza98", "hasza98@gmail.com", "password", AuthorityUtils.createAuthorityList("ROLE_USER"));
@@ -264,6 +505,19 @@ class CaffControllerTest {
                 .andExpect(status().isForbidden());
     }
 
+    /**
+     * Description:
+     *      Checking if getting the parsed image of the given caff returns the image.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return true for finding the caff with the specified id.
+     *      Upon finding it, it should return the caff wrapped as Optional.
+     * Request:
+     *      GET(/caffs/unauth/image/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Image in the response, indicated in the headers.
+     */
     @Test
     void getCaffImage_Success() throws Exception {
         Caff caffOne = new Caff("a","b",new Binary(new byte[0]));
@@ -276,6 +530,19 @@ class CaffControllerTest {
                 .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + caffOne.getName() + ".gif\""));
     }
 
+    /**
+     * Description:
+     *      Checking if getting the parsed image of the given caff returns the correct error, if the caff with the id doesnt exist.
+     * Mock Restrictions:
+     *      CaffRepository is mocked, it should return false for finding the caff with the specified id.
+     * Request:
+     *      GET(/caffs/unauth/image/1)
+     * Input:
+     *      Path parameter ID (1)
+     * Expected output:
+     *      Bad request, with message: Caff does not exist!
+     *      caffRepository.findById never called with argument "1"
+     */
     @Test
     void getCaffImage_CaffNotFound() throws Exception {
         Caff caffOne = new Caff("a","b",new Binary(new byte[0]));
